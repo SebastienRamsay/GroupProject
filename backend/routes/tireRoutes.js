f775af4a81178d04fa032bff15eeb9ff85b5785c
Sebastien Ramsay <ramsa0130@algonquinlive.com>
2023-03-21 15:02:58 -0400

    Fixed bug and renamed tireRoutes

--- backend/routes/tireRoutes.js
+++ backend/routes/tireRoutes.js
@@ -5,27 +5,27 @@ const router = express.Router()
 
 // GET all tires
 router.get('/', (req, res) => {
-  res.json(Controller.getTire())
+  res.json(Controller.getTires)
 })
 
 // GET a single tire
 router.get('/:id', (req, res) => {
-  res.json(Controller.getTires())
+  res.json(Controller.getTire)
 })
 
 // POST a new tire
 router.post('/', (req, res) => {
-  res.json(Controller.createTire())
+  res.json(Controller.createTire)
 })
 
 // DELETE a tire
 router.delete('/:id', (req, res) => {
-  res.json(Controller.deleteTire())
+  res.json(Controller.deleteTire)
 })
 
 // UPDATE a tire
 router.patch('/:id', (req, res) => {
-  res.json(Controller.updateTire())
+  res.json(Controller.updateTire)
 })
 
 module.exports = router
