b1f3ae82534228673afbee6c2e8506822db6e007
AyaAlsadi95 <97901459+AyaAlsadi95@users.noreply.github.com>
2023-03-21 14:20:21 -0400

    Schema

---
+++ backend/models/Tire.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const tireSchema = new mongoose.Schema({
+  brand: {
+    type: String,
+    required: true
+  },
+  model: {
+    type: String,
+    required: true
+  },
+  size: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  isAvailable: {
+    type: Boolean,
+    default: true
+  }
+});
+
+module.exports = mongoose.model('Tire', tireSchema);
